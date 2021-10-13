04ea89aadaade96cb4cd92fa259564fe59444568
Alexei Utkin <lith@protonmail.ch>
2021-10-13 19:02:38 +1000

    Fix Modal window on edit/delete

---
+++ resources/views/components/alert-unable-delete.blade.php
@@ -0,0 +1,17 @@
+<div class="modal fade" id="cannotDeleteModal" tabindex="-1" role="dialog" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        {{ $message }}
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Отмена</button>
+      </div>
+    </div>
+  </div>
+</div>
